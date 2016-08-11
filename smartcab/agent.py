9ae5912b71a012722469e052d132204a4c976d7c
Omoju <omojumiller@gmail.com>
2016-08-10 19:08:11 -0700

    feat: Update 'State'

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -27,12 +27,20 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
+        ## currently using two states called random and initiated
+        if(self.state == None):
+            self.state = 'Some State'
+
+
+
+
 
         # TODO: Select action according to your policy
         action = random.choice([None, 'forward', 'left', 'right'])
 
         # Execute action and get reward
         reward = self.env.act(self, action)
+
 
         # TODO: Learn policy based on state, action, reward
 
@@ -45,14 +53,14 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0.5, display=False)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
-    sim.run(n_trials=10)  # run for a specified number of trials
+    sim.run(n_trials=100)  # run for a specified number of trials
     # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
 
 
